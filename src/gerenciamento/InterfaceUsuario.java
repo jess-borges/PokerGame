b61afb23ac2602420890107dcf0c42c019596633
lucaspMACHADO <lucaspedro.machado@gmail.com>
2016-05-11 23:46:09 -0300

    Métodos da classe InterfaceUsuario adicionados

--- src/gerenciamento/InterfaceUsuario.java
+++ src/gerenciamento/InterfaceUsuario.java
@@ -1,5 +1,46 @@
 package gerenciamento;
 
-public class InterfaceUsuario {
+import java.util.ArrayList;
+import plano.Jogador;
+import plano.Carta;
 
+public class InterfaceUsuario {
+	
+	static String stringCorrente;
+	
+	public static void printBoasVindas() {
+		//
+	}
+	
+	public static int getNumeroJogadores() {
+		//
+	}
+	
+	public static String getNomeJogador() {
+		//
+	}
+	
+	public static int getNumeroFichasPilha() {
+		//
+	}
+	
+	public static void printQuadroJogadores(ArrayList<Jogador> jogadores, int idDealer) {
+		//
+	}
+	
+	public static void printApostas(int pote, ArrayList<Jogador> jogadores, int idDealer) {
+		//
+	}
+	
+	public static ArrayList<Carta> getCartasDraw() {
+		//
+	}
+	
+	public static int decifraComando() {
+		//
+	}
+	
+	public static void printShowDown(ArrayList<Jogador> jogadores, int idVencedor, int pote) {
+		//
+	}
 }
